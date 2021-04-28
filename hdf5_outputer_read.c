fdc6959f4043f158034814712eb66220fabedb4a
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-28 15:34:36 -0500

    Update hdf5_outputer_read.c

--- hdf5_outputer_read.c
+++ hdf5_outputer_read.c
@@ -161,7 +161,7 @@ int scan_attributes(hid_t hid, char*** attribute_names, char*** attribute_bufs, 
 	attribute_bufs[0][i] = (char*) malloc(esize * dims[0]);
         attribute_types[0][i] = tid;
 	attribute_sizes[0][i] = dims[0];
-        printf("read attribute with name %s, size = %llu, dim size = %llu\n", attribute_name, (long long unsigned) ainfo.data_size, (long long unsigned)(esize * dims[0]));
+        printf("read attribute with name %s, size = %llu, ndim = %d, dim size = %llu\n", attribute_name, (long long unsigned) ainfo.data_size, ndim, (long long unsigned)dims[0] );
 	//err = H5Aread(aid, tid, attribute_bufs[0][i] );
 	if (err < 0) {
 	    printf("error code = %lld\n", (long long int) err);
