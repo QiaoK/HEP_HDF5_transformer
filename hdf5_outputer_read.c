eac968260274b0d090fd9cfd14ef5b31acd0c4af
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-29 01:06:08 -0500

    Update hdf5_outputer_read.c

--- hdf5_outputer_read.c
+++ hdf5_outputer_read.c
@@ -328,6 +328,11 @@ int main (int argc, char **argv) {
     char out_filename[256];
     int use_core_driver;
 
+    char **attribute_names = NULL, **attribute_bufs = NULL;
+    hsize_t *attribute_sizes = NULL;
+    hid_t *attribute_types = NULL;
+    int n_attributes = 0;
+
     if (argc != 3 ) { 
         printf("Usage: ./test filename core_driver\n");
         return 1;
@@ -343,6 +348,16 @@ int main (int argc, char **argv) {
 
     faplid = H5Pcreate (H5P_FILE_ACCESS);
     file = H5Fopen (argv[1], H5F_ACC_RDONLY, faplid);
+
+    scan_attributes(file, &attribute_names, &attribute_bufs, &attribute_sizes, &attribute_types, &n_attributes);
+    printf("number of attributes is %d\n", n_attributes);
+
+    char **attribute_names = NULL, **attribute_bufs = NULL;
+    hsize_t *attribute_sizes = NULL;
+    hid_t *attribute_types = NULL;
+    int n_attributes = 0;
+
+
     sprintf(out_filename, "%s.copy", argv[1]);
 
     if ( use_core_driver ) { 
