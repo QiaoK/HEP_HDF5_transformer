c1792c15d0efaad7a716ee4a959d68e44bbd85ea
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-28 21:55:31 -0500

    Update hdf5_outputer_read.c

--- hdf5_outputer_read.c
+++ hdf5_outputer_read.c
@@ -75,6 +75,8 @@ int scan_datasets(hid_t out_gid, hid_t gid, hid_t **dataset_list, size_t *datase
                 //printf(" GROUP: %s\n", memb_name);
                 grpid = H5Gopen(gid, memb_name, H5P_DEFAULT);
                 scan_datasets(out_grpid, grpid, dataset_list, dataset_list_size, dataset_list_max_size);
+		    scan_attributes(grpid, &attribute_names, &attribute_bufs, &attribute_sizes, &attribute_types, &n_attributes);
+
                 H5Gclose(grpid);
                 break;
             }
@@ -82,12 +84,12 @@ int scan_datasets(hid_t out_gid, hid_t gid, hid_t **dataset_list, size_t *datase
                 //printf(" DATASET: %s\n", memb_name);KE
                 /* Open a dataset from the current group */
                 did = H5Dopen(gid, memb_name, H5P_DEFAULT);
+		    scan_attributes(did, &attribute_names, &attribute_bufs, &attribute_sizes, &attribute_types, &n_attributes);
                 /* We write the dataset into our new file*/
 		if ( out_grpid >= 0 ) {
                     dsid = H5Dget_space (did);
                     tid = H5Dget_type(did);
                     fetch_data(did, &buf, &buf_size);
-		    scan_attributes(did, &attribute_names, &attribute_bufs, &attribute_sizes, &attribute_types, &n_attributes);
 		    write_data(buf, buf_size, memb_name, out_grpid, tid, datasets + dataset_index, attribute_names, attribute_bufs, attribute_sizes, attribute_types, n_attributes);
                     dataset_index++;
                     H5Sclose(dsid);
@@ -293,12 +295,12 @@ int write_data(char *buf, hsize_t buf_size, char *dataset_name, hid_t out_id, hi
 */
     gettimeofday(&end_time, NULL);
     metadata_write_time += (end_time.tv_usec + end_time.tv_sec * 1000000 - start_time.tv_usec - start_time.tv_sec * 1000000) * 1.0;
-
-    free(attribute_names);
-    free(attribute_bufs);
-    free(attribute_sizes);
-    free(attribute_types);
-
+    if ( n_attributes ) {
+        free(attribute_names);
+        free(attribute_bufs);
+        free(attribute_sizes);
+        free(attribute_types);
+    }
 
     gettimeofday(&start_time, NULL);
     gettimeofday(&end_time, NULL);
